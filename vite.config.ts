3d44f72452a1054835415cad6e15f787404303e0
KrisWis <kworkEvgeniy@yandex.ru>
2024-12-08 14:50:24 +0300

    Fix bug with scroll.

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,19 @@ export default defineConfig({
   build: {
     emptyOutDir: false,
     outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
   server: {
     open: true,
