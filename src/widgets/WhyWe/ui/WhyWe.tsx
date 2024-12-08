6d2e4931291ba242c63ccdec1ab76e3f33cad07f
KrisWis <kworkEvgeniy@yandex.ru>
2024-12-08 18:15:41 +0300

    Add start screen.

--- src/widgets/WhyWe/ui/WhyWe.tsx
+++ src/widgets/WhyWe/ui/WhyWe.tsx
@@ -1,6 +1,23 @@
+import { Flex } from "@/shared/lib/Stack";
 import styles from "./WhyWe.module.scss";
-import { memo } from "react";
+import { memo, useContext } from "react";
+import SpinnedLineSVG from "@/shared/assets/icons/global/SpinnedLineSVG.svg?react";
+import { MainPageContext } from "@/pages/MainPage";
+import { Button } from "@/shared/ui-kit/Button";
 
 export const WhyWe: React.FC = memo((): React.JSX.Element => {
-  return <div id="WhyWe" className={styles.WhyWe}></div>;
+  const { scrollPosition } = useContext(MainPageContext);
+
+  return (
+    <Flex direction="column" id="WhyWe" className={styles.WhyWe}>
+      <h2 className={styles.WhyWe__caption}>
+        ПОЧЕМУ ВЫ <br /> ВЫБЕРЕТЕ НАС?
+        {scrollPosition == "WhyWe" && (
+          <SpinnedLineSVG className={styles.WhyWe__caption__line} />
+        )}
+      </h2>
+
+      <Button themes={["withBG", "big-x-padding"]}>Почему?</Button>
+    </Flex>
+  );
 });
