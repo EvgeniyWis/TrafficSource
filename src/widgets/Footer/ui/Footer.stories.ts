7098b6239e8492e6a09bbe68025fd34f574df867
KrisWis <kworkEvgeniy@yandex.ru>
2024-12-09 22:03:23 +0300

    Fix stories.

--- src/widgets/Footer/ui/Footer.stories.ts
+++ src/widgets/Footer/ui/Footer.stories.ts
@@ -1,13 +1,9 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Footer } from './Footer';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Footer } from "./Footer";
 
 const meta = {
-  title: 'Widgets/Footer',
+  title: "Widgets/Footer",
   component: Footer,
-  parameters: {
-    layout: 'centered',
-  },
-
 } satisfies Meta<typeof Footer>;
 
 export default meta;
